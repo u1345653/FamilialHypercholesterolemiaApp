5d1f3eae374c3d5f57ed8e749b7855ee90bb1056
u1345653 <u1345653@umail.utah.edu>
2022-01-26 12:50:23 -0700

    v1.12 - Added request import to flask class, and accounted for request that includes a POST request.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template('index.html')
+    if request.method == "POST":
+        data = request.data
+        print("Python Data: ", data)
+        return render_template('index.html', data = data)
+
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=False)
